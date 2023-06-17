49cddd7504aee24d381355d26e73679f5ce64157
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-06-17 05:30:51 -0400

    rich embed for discord noti

--- src/lib/connection/discord.ts
+++ src/lib/connection/discord.ts
@@ -1,5 +1,11 @@
 import { Connection, Platform, DiscordConnectionOptions } from "./connection.js";
 
+type DiscordMessage = {
+    from: string,
+    title: string,
+    message: string
+}
+
 export default class DiscordConnection extends Connection {
     webhook: URL;
     opts: DiscordConnectionOptions;
@@ -10,14 +16,22 @@ export default class DiscordConnection extends Connection {
         this.webhook = new URL(opts.webhook)
     }
 
-    async send(message: any) {
+    send(data: DiscordMessage) {
         fetch(this.webhook, {
             method: "POST",
             headers: {
                 "content-type": "application/json"  
             },
             body: JSON.stringify({
-                content: message
+                embeds: [
+                    {
+                        title: data.title,
+                        author: {
+                            name: data.from
+                        },
+                        description: data.message
+                    }
+                ]
             })
         })
     }
