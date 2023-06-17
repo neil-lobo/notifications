a194ca4607b63356da798e7e6f33774662420ff2
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-06-17 06:08:39 -0400

    convert list to map of connections

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,23 @@ import { Connection } from "./lib/connection/connection.js";
 import HTTPConnection from "./lib/connection/http.js";
 import { TwitchConnection, DiscordConnection } from "./lib/connections.js";
 
-const connections: Connection[] = [];
+const connections = new Map<string, Connection>();
 
 for(let connection of config.connections) {
     switch(connection.platform) {
         case "TWITCH": {
             const conn = new TwitchConnection(connection.label, connection.options);
-            connections.push(conn);
+            connections.set(connection.label, conn);
             break;
         }
         case "DISCORD": {
             const conn = new DiscordConnection(connection.label, connection.options);
-            connections.push(conn)
+            connections.set(connection.label, conn)
             break;
         }
         case "HTTP": {
             const conn = new HTTPConnection(connection.label, connection.options);
-            connections.push(conn)
+            connections.set(connection.label, conn)
             break;
         }
     }
