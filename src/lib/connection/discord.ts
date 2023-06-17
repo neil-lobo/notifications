ceb895aa5055401dae5eb7f776f1b72d98936dbd
Neil Lobo <neil_edward_lobo@hotmail.com>
2023-06-17 05:07:10 -0400

    implement discord send

--- src/lib/connection/discord.ts
+++ src/lib/connection/discord.ts
@@ -1,11 +1,22 @@
 import { Connection, Platform, DiscordConnectionOptions } from "./connection.js";
 
 export default class DiscordConnection extends Connection {
+    webhook: URL;
+
     constructor(opts: DiscordConnectionOptions) {
         super(Platform.DISCORD, opts);
+        this.webhook = new URL(opts.webhook)
     }
 
-    send(message: any) {
-        console.log(message);
+    async send(message: any) {
+        fetch(this.webhook, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"  
+            },
+            body: JSON.stringify({
+                content: message
+            })
+        })
     }
 }
